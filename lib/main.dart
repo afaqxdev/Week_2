2db25536c31672fb717e51e7ebb31d9cae2e36ff
Afaq Zahir <116549225+MetasenseDevelopment@users.noreply.github.com>
2025-02-01 13:02:48 +0500

    stack ,expanded,responsiveness,layoutbuilder

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 // import 'package:flutter/material.dart';
 
 import 'package:flutter/material.dart';
-import 'package:week_2/home_screen.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'home_screen/mobile_screen.dart';
 
 void main() {
-  runApp(const MaterialApp(home: HomeScreen1()));
+  runApp(const ScreenUtilInit(
+    child: MaterialApp(
+      home: HomeScreenMobile(),
+      debugShowCheckedModeBanner: false,
+    ),
+  ));
 }
 
 
@@ -38,3 +45,45 @@ void main() {
   //                 onTap: () {},
   //                 child: const Text("orgot Password"),
   //               )
+
+
+
+//  const Row(
+//                   children: [
+//                     Text(
+//                       "data",
+//                     ),
+//                     SizedBox(
+//                       width: 10,
+//                     ),
+//                     Expanded(
+//                       child: Text(
+//                           "afsasfsajfkjsdhfksdhfkhfkjsdhfkjashfaskfhaskdhkasjhdkajshdkasj"),
+//                     )
+//                   ],
+//                 ),
+
+    //  Stack(
+    //               children: [
+    //                 Container(
+    //                   width: 110,
+    //                   height: 100,
+    //                   color: Colors.red,
+    //                 ),
+    //                 Container(
+    //                   width: 30,
+    //                   height: 49,
+    //                   color: Colors.green,
+    //                 ),
+    //                 Positioned(
+    //                   bottom: 10,
+    //                   right: 0,
+    //                   top: 80,
+    //                   child: Container(
+    //                     width: 10,
+    //                     height: 20,
+    //                     color: Colors.black,
+    //                   ),
+    //                 ),
+    //               ],
+    //             ),
